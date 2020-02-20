0abb0c7b4a525dd9d7f5f2f741454450cb2e562f
sjoseph1848 <sjoseph1848@gmail.com>
2020-02-19 22:58:57 -0600

    valueChanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { DemoOneComponent } from './components/demo-one/demo-one.component';
 import { HomeComponent } from './components/home/home.component';
 import { DemoTwoComponent } from './components/demo-two/demo-two.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/demo-one/demo-one.component.ts
+++ src/app/components/demo-one/demo-one.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { initializeApp, database } from 'firebase';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-demo-one',
@@ -6,10 +11,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo-one.component.css']
 })
 export class DemoOneComponent implements OnInit {
-
-  constructor() { }
+  items: Observable<any[]>
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
+    this.db.collection('passengers').valueChanges().subscribe(val => console.log(val))
   }
 
 }
